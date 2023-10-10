3837468c11be68f83528f964ecaf2a8110ab019b
RonCondas <124211322+RonCondas@users.noreply.github.com>
2023-10-10 11:32:34 -0500

    Add files via upload

---
+++ prac.java
@@ -0,0 +1,44 @@
+package chapter3prac;
+
+import java.util.Scanner;
+
+public class prac {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner kb = new Scanner(System.in);
+//		System.out.println(Math.PI);
+//		
+//		System.out.println(Math.abs(-30));
+//		
+//		System.out.println(Math.min(100, Math.min(20, Math.min(30, 45))));
+//		
+//		
+//		System.out.println(Math.min(10,11.5));
+		
+//		gift1 = playstation5;
+//		
+//		gift2 = iphone;
+//		
+//		gift1 = true;
+//		
+//		gift2 = false;
+//		
+//		gift1 & gift2 = childhappy;
+		
+		
+//		int num1 = 0;
+//		int num2 =0;
+//		
+//		System.out.println("Enter the first number: ");
+//		num1 = kb.nextInt();
+//		
+//		System.out.println("Enter the second number: ");
+//		num2 = kb.nextInt();
+//		
+//		while (!(Character.isDigit()));
+//		
+	}
+
+}
